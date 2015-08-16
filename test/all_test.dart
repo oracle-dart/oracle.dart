da81dc86ae796d43290eb3b4a366c48ad90455f0
Chris Lucas <chris@chrisjlucas.com>
2015-08-16 11:43:32 -0700

    Deleted all_test.dart

--- test/all_test.dart
+++
@@ -1,9 +0,0 @@
-import 'statement_test.dart' as statement_test;
-import 'resultset_test.dart' as resultset_test;
-import 'metadata_test.dart' as metadata_test;
-
-main() {
-  statement_test.main();
-  resultset_test.main();
-  metadata_test.main();
-}
