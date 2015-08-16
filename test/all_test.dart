63413bdfc58ee166afb4ab4035adaa5f247c1deb
Chris Lucas <chris@chrisjlucas.com>
2015-08-16 01:22:00 -0700

    Added metadata test

--- test/all_test.dart
+++ test/all_test.dart
@@ -1,7 +1,9 @@
 import 'statement_test.dart' as statement_test;
 import 'resultset_test.dart' as resultset_test;
+import 'metadata_test.dart' as metadata_test;
 
 main() {
   statement_test.main();
   resultset_test.main();
+  metadata_test.main();
 }
