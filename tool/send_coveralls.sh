f1642c8086a02e740f069e7317f58034195ca384
Chris Lucas <chris@chrisjlucas.com>
2015-08-16 12:55:41 -0700

    Added logging

--- tool/send_coveralls.sh
+++ tool/send_coveralls.sh
@@ -12,5 +12,8 @@ echo "Covering cpp"
 cpp-coveralls -b lib/src -t faketoken --dump coverage/cpp_coverage.json
 
 merged_json=coverage/merged.json
+echo "Merging coverage files"
 python tool/coveralls_merge.py coverage/* $merged_json
+
+echo 'Uploading to coveralls.io...'
 curl -v -F json_file=@$merged_json https://coveralls.io/api/v1/jobs
