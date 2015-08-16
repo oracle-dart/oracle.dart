608ba78d9f45accd4d2bd4f83c3979ddfebeebe7
Chris Lucas <chris@chrisjlucas.com>
2015-08-16 12:31:31 -0700

    Remove test files from dart coverage

--- tool/send_coveralls.sh
+++ tool/send_coveralls.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+rm -rf coverage > /dev/null
 mkdir coverage
 
 for f in test/*_test.dart; do
-    dart_coveralls report --dry-run -p $f | python tool/scrape_json.py > coverage/$(basename $f).json
+    echo "Covering $f"
+    dart_coveralls report -T --dry-run -p $f | python tool/scrape_json.py > coverage/$(basename $f).json
 done
 
+echo "Covering cpp"
 cpp-coveralls -b lib/src -t faketoken --dump coverage/cpp_coverage.json
 
 python tool/coveralls_merge.py coverage/*
