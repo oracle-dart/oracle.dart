a809622970692bbc376a3775cada9ac82b4723bf
Chris Lucas <chris@chrisjlucas.com>
2015-08-16 12:27:08 -0700

    fixed cpp coveralls bin name

--- tool/send_coveralls.sh
+++ tool/send_coveralls.sh
@@ -6,4 +6,6 @@ for f in test/*_test.dart; do
     dart_coveralls report --dry-run -p $f | python tool/scrape_json.py > coverage/$(basename $f).json
 done
 
-cpp-coverage -b lib/src -t faketoken --dump coverage/cpp_coverage.json
+cpp-coveralls -b lib/src -t faketoken --dump coverage/cpp_coverage.json
+
+python tool/coveralls_merge.py coverage/*
