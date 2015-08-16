348adb67306427dedd29ca1522eaa3f44cfa4e8f
Chris Lucas <chris@chrisjlucas.com>
2015-08-16 12:25:50 -0700

    Added python

--- tool/send_coveralls.sh
+++ tool/send_coveralls.sh
@@ -3,7 +3,7 @@
 mkdir coverage
 
 for f in test/*_test.dart; do
-    dart_coveralls report --dry-run -p $f | tool/scrape_json.py > coverage/$(basename $f).json
+    dart_coveralls report --dry-run -p $f | python tool/scrape_json.py > coverage/$(basename $f).json
 done
 
 cpp-coverage -b lib/src -t faketoken --dump coverage/cpp_coverage.json
